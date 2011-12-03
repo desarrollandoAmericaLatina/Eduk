53f521bc5fc74e39cef9f0686f48d70a30341d13
Andres Espinace <atarix.010101@gmail.com>
2011-12-03 19:19:18 -0300

    antes del branch dev

--- main/models.py
+++ main/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.contrib.auth import User
 # Create your models here.
+
+class usuario(models.Model):
+    def __unicode__(self):
+        return self.username
+
+class establecimiento(models.Model):
+    codigo = models.CharField(max_length=100, verbose_name='Codigo de identificacion', null=False, help_text='Codigo de identificacion nem')
+    nombre = models.CharField(max_length=50, verbose_name='Nombre', null=False)
+    ubicacion = models.CharField(max_length=100, verbose_name='Direccion', null=True, help_text='(eg. direccion numero, comuna, ciudad)')
+
